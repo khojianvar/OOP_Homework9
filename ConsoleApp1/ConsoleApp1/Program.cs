34f6beb1bc3e0b3ffd0acb9aed14875af4f2661a
khojianvar <khojianvarabdumukhtorov@gmail.com>
2023-05-10 13:21:05 +0500

    task3

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,10 +1,34 @@
-﻿namespace ConsoleApp1
+﻿using System.Net;
+
+namespace ConsoleApp1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<Student> listStudents = new List<Student>();
+            listStudents.Add(new Student(1, "dafa", "safdas"));
+            listStudents.Add(new Student(2, "sdafa", "asafdas"));
+            foreach (Student student in listStudents)
+            {
+                Console.WriteLine(student);
+            }
         }
+    }
+    class Student 
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string PhoneNumber { get; set; }
+        public Student(int id, string fullName, string phoneNumber) 
+        {
+            Id = id;
+            FullName = fullName;
+            PhoneNumber = phoneNumber;
+        }
+    }
+    class Group 
+    {
+        public Student student { get; set; }
     }
 }
